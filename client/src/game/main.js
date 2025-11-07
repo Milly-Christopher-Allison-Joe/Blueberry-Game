7d9ecb69cecaa084bca6277014b78750352be035
Joe <jlucks1177@gmail.com>
2025-11-07 16:23:18 -0600

    First major commit. Wired up the initial game files and got the game to show up on the browser. Additional work to be done later

---
+++ client/src/game/main.js
@@ -0,0 +1,21 @@
+import { Boot } from "./scenes/Boot";
+import { Plexus } from "./scenes/Plexus";
+import { GameOver } from "./scenes/GameOver";
+import { MainMenu } from "./scenes/MainMenu";
+import Phaser from "phaser";
+import { Preloader } from "./scenes/Preloader";
+
+const config = {
+  type: Phaser.AUTO,
+  width: 1024,
+  height: 768,
+  parent: "game-container",
+  backgroundColor: "#028af8",
+  scene: [Boot, Preloader, MainMenu, Plexus, GameOver],
+};
+
+const StartGame = (parent) => {
+  return new Phaser.Game({ ...config, parent });
+};
+
+export default StartGame;
---
+++ client/src/game/scenes/MainMenu.js
@@ -0,0 +1,45 @@
+import { Scene } from "phaser";
+
+export class MainMenu extends Scene {
+  constructor() {
+    super("MainMenu");
+  }
+
+  create() {
+    // Center of the screen
+    const { width, height } = this.cameras.main;
+
+    // Draw the background, centered
+    const bg = this.add.image(width / 2, height / 2, "background");
+    bg.setOrigin(0.5);
+
+    // Scale background to fill screen but maintain aspect ratio
+    const scaleX = width / bg.width;
+    const scaleY = height / bg.height;
+    const scale = Math.max(scaleX, scaleY);
+    bg.setScale(scale);
+
+    // Title text
+    this.add
+      .text(width / 2, height / 2 - 100, "BlueBerry Game", {
+        fontFamily: "Arial",
+        fontSize: "48px",
+        color: "#ffffff",
+      })
+      .setOrigin(0.5);
+
+    // Start prompt
+    this.add
+      .text(width / 2, height / 2 + 20, "Press SPACE to Begin", {
+        fontFamily: "Arial",
+        fontSize: "24px",
+        color: "#bbbbbb",
+      })
+      .setOrigin(0.5);
+
+    // Input listener
+    this.input.keyboard.once("keydown-SPACE", () => {
+      this.scene.start("Plexus");
+    });
+  }
+}
