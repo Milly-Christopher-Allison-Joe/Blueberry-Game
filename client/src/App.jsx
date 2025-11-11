09370efa109b5e07fc1a7694ec6ca299107bf76a
millymontes <millybriannaa@gmail.com>
2025-11-10 18:48:46 -0700

    added auth folder with authcontext,error404,login,register. added layout folder with layout,navbar. added pages folder with about,home,profile. added css to the index.css. completed what it takes to render the navbar onto the webpage with app.jsx and main.jsx. altered index.html a little to help with the rendering.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,36 +1,25 @@
-import { Routes, Route } from "react-router-dom";
-import { useRef } from "react";
-import Navbar from "./components/Navbar.jsx";
-import { PhaserGame } from "./Phaser.jsx";
-
-import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
 import Register from "./auth/Register.jsx";
 import Login from "./auth/Login.jsx";
 import Profile from "./pages/Profile.jsx";
 import Error404 from "./auth/Error404.jsx";
+import { Routes, Route } from "react-router-dom";
+import { useRef } from "react";
+import { PhaserGame } from "./Phaser.jsx";
 
 export default function App() {
-  const gameRef = useRef();
+  const phaserRef = useRef(null);
 
   return (
     <div className="app-container">
-      <Navbar />
       <Routes>
-        <Route
-          path="/"
-          element={
-            <div className="game-wrapper">
-              <PhaserGame ref={gameRef} />
-            </div>
-          }
-        />
         <Route path="/about" element={<About />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="*" element={<Error404 />} />
       </Routes>
+      <PhaserGame ref={phaserRef} />
     </div>
   );
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,18 +1,18 @@
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import Layout from "./layout/Layout.jsx";
 
+import { AuthProvider } from "./auth/AuthContext.jsx";
 import { BrowserRouter } from "react-router-dom";
 
-import { AuthProvider } from "./auth/AuthContext";
-import { StrictMode } from "react";
-
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <BrowserRouter>
     <AuthProvider>
-      <BrowserRouter>
+      <Layout>
         <App />
-      </BrowserRouter>
+      </Layout>
     </AuthProvider>
-  </StrictMode>
+  </BrowserRouter>
 );
