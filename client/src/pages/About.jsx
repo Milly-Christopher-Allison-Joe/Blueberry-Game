0d0e8190d3dbaf9370cee2759e93e9b6a8e02d0a
millymontes <millybriannaa@gmail.com>
2025-12-01 18:12:41 -0700

    added text for the about us section as well as adding a folder with all of our pictures.

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -12,8 +12,26 @@ export default function About() {
       </p>
 
       <h2>Divison of workload</h2>
-      <p> do we want to add this section? </p>
-
+      <h3 className="team-member">Joe Loverde</h3>
+      <p>
+        Focused on most of the game play mechanics itself as well as setting up
+        the scenes, menus, boss mechanics and bosses.
+      </p>
+      <h3 className="team-member">Christopher Wright</h3>
+      <p>
+        Worked on player machanics, backend with high scores and setting up
+        deployment.
+      </p>
+      <h3 className="team-member">Allison Andres</h3>
+      <p>
+        Focused on setting up the database, account registration, user login,
+        and the routes.
+      </p>
+      <h3 className="team-member">Milly Montes</h3>
+      <p>
+        Worked on the styling of the website, creating the About and Home page,
+        created the user and boss sprites.
+      </p>
       <h2>Contact</h2>
       <p>
         Do you have feedback that will improve your gaming experience or did you
