3d70e83dd1f343da19a7ffc0f52a394b709f746f
Christopher Wright <chrisalanwright@gmail.com>
2025-12-01 21:57:14 -0500

    set up for heroku

--- server.js
+++ server.js
@@ -7,10 +7,19 @@ import "dotenv/config";
 import usersRouter from "./server/api/users.js";
 import bossesRouter from "./server/api/boss.js";
 import highscoreRouter from "./server/api/highscore.js";
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const PORT = 3000;
 
 await db.connect();
+
+app.use(express.static(path.join(__dirname, "client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/dist", "index.html"));
+});
 
 app.use("/api/users", usersRouter);
 app.use("/api/bosses", bossesRouter);
